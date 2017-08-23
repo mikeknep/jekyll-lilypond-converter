89eb8832e2edde19bb46e9a8f08dc4277a799999
Mike Knepper <michaelrknepper@gmail.com>
2017-08-22 19:33:07 -0500

    Add singleton to store reference to Jekyll::Site
    The Jekyll::Site object keeps track of static files to add to the
    generated _site. It is accessible via Generators, but not Converters.
    However, it doesn't make sense to parse through all the posts at the
    Generator level when we'll have much more control over that parsing at
    the Converter level. So, add a Generator simply to get a reference to
    the Jekyll::Site, store it on a singleton, and then access that site in
    the Converter to add individual image files.

---
+++ lib/site_manager.rb
@@ -0,0 +1,20 @@
+require "jekyll"
+require "singleton"
+
+module JekyllLilyPondConverter
+  class SiteManager
+    include Singleton
+
+    attr_accessor :site
+
+    def add_image(filename)
+      site.static_files << Jekyll::StaticFile.new(
+        site,
+        site.source,
+        "images",
+        filename
+      )
+    end
+
+  end
+end
